5fe983dc1571338c4d0773bfd181698520d56e3b
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-21 15:57:31 +0330

    use LoginForm to App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import LoginForm from "./components/LoginForm/index.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <LoginForm/>
+      </div>
+    </>
+  );
+};
+
+export default App;
