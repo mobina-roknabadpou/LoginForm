0d42b7f8c76d570c6f18c48128ddba3e00ff5888
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-21 17:40:45 +0330

    fix: fixed email validation

--- src/components/PattenValidation/validation.js
+++ src/components/PattenValidation/validation.js
@@ -1,3 +1,3 @@
 export const VALID_PASSWORD =
   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-export const VALID_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{3}$/;
+export const VALID_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
