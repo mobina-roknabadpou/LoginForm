6cb049d70673cdfec0a41d0bada36ddfc73f566c
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-21 15:49:41 +0330

    feat: created Inputcomponent

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./style.module.css";
+
+function Input({ name, id, type, state, setState }) {
+  console.log("value: ", state);
+  return (
+    <div className={style.container}>
+      <label htmlFor={id} className={style.label}>
+        {name}
+      </label>
+      <input
+        type={type}
+        id={id}
+        className={style.inputForm}
+        value={state.value}
+        onChange={(e) => setState({ ...state, value: e.target.value })}
+        required
+        autocomplete={outoComplete}
+      />
+    </div>
+  );
+}
+
+export default Input;
