7dbd72a3ffae0f058ce0975cfaf473e837d10de4
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-23 18:19:18 +0330

    feat: added new state and method

--- src/components/FormControl/index.jsx
+++ src/components/FormControl/index.jsx
@@ -1,56 +1,92 @@
 import React from "react";
 import Options from "../Options/index.jsx";
-import Input from "../Input/index.jsx";
-import { notifyError } from "../Notify/notify.js";
+import style from "./style.module.css";
 import { postData } from "../../services/postInfo.js";
+import { useState } from "react";
+import { Icon } from "react-icons-kit";
+import { eyeDisabled } from "react-icons-kit/ionicons/eyeDisabled";
+import { eye } from "react-icons-kit/ionicons/eye";
 
 const FormControl = ({
   email,
   setEmail,
   password,
   setPassword,
-  form,
-  btnSubmit,
-  optionItem,
+  classNameForm,
+  classNameBtnSubmit,
+  classNameOptionItem,
+  showToastNotification,
 }) => {
-  
+  const [type, setType] = useState("password");
+  const [isLoading, setIsloading] = useState(false);
+  const icon = type === "password" ? eyeDisabled : eye;
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!email.isValidate) {
-      notifyError("فرمت ایمیل صحیح نیست!");
+    if (!email.isValid) {
+      showToastNotification("فرمت ایمیل صحیح نیست", true);
       return;
     }
     const info = {
       UserName: email.value,
       password: password.value,
     };
-    postData(info);
+    postData(info, showToastNotification, setIsloading);
   };
 
   return (
     <>
-      <form className={form} onSubmit={handleSubmit}>
-        <Input
-          name="ایمیل"
-          type="text"
-          id="email"
-          state={email}
-          setState={setEmail}
-        />
-        <Input
-          name="رمز عبور"
-          type="password"
-          id="password"
-          state={password}
-          setState={setPassword}
-        />
+      <form className={classNameForm} onSubmit={handleSubmit}>
+        <div className={style.container}>
+          <label htmlFor="email" className={style.label}>
+            ایمیل
+          </label>
+          <input
+            type="text"
+            id="email"
+            className={style.inputForm}
+            value={email.value}
+            onChange={(e) => setEmail({ ...email, value: e.target.value })}
+            required
+            autoComplete="off"
+          />
+        </div>
+        <div className={style.container}>
+          <label htmlFor="password" className={style.label}>
+            رمز عبور
+          </label>
+          <input
+            type={type}
+            id="password"
+            className={style.inputForm}
+            value={password.value}
+            onChange={(e) =>
+              setPassword({ ...password, value: e.target.value })
+            }
+            required
+            autoComplete="off"
+          />
+          {password.value && (
+            <span onClick={handleToggle} className={style.icon}>
+              <Icon icon={icon} size={24} className={style.eyeIcon} />
+            </span>
+          )}
+        </div>
         <button
-          className={btnSubmit}
-          disabled={!email.value || !password.value}
+          className={classNameBtnSubmit}
+          disabled={!isLoading && (!email.value || !password.value)}
         >
-          ورود
+          {!isLoading ? <p>ورود</p> : "isloading"}
         </button>
-        <Options optionItem={optionItem} />
+        <Options optionItem={classNameOptionItem} />
       </form>
     </>
   );
