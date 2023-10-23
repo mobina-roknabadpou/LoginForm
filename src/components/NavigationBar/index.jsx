025148234c6efd5626ea136d51c4d3c21cb35524
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-23 18:21:26 +0330

    use Toast in NavigationBar component

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Logo from "../Icons/Logo/index.jsx";
-import style from './style.module.css'
+import style from "./style.module.css";
+import Toast from "../Toast/index.jsx";
 
-function NavigationBar() {
+function NavigationBar({ toastMessage, showToast, closeToast, flag }) {
   return (
     <div className={style.container}>
       <div className={style.auth}>
         <Logo />
+        <Toast
+          message={toastMessage}
+          showToast={showToast}
+          onClose={closeToast}
+          flag={flag}
+        />
         <button className={style.btn}>ثبت نام</button>
       </div>
     </div>
