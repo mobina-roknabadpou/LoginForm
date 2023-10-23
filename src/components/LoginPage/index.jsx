ad6f403382847fc880d5f5a72e3746867be547f1
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-10-23 17:32:03 +0330

    rename: rename Folder

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -3,29 +3,46 @@ import style from "./style.module.css";
 import NavigationBar from "../NavigationBar/index.jsx";
 import { useState, useEffect } from "react";
 import { VALID_EMAIL } from "../PattenValidation/validation";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import FormControl from "../FormControl/index.jsx";
 
 function LoginForm() {
   const [email, setEmail] = useState({
     value: "",
-    isValidate: false,
+    isValid: false,
   });
 
   const [password, setPassword] = useState({
     value: "",
   });
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [flag, setFlag] = useState(true);
+  const showToastNotification = (message, flag) => {
+    setToastMessage(message);
+    setShowToast(true);
+    setFlag(flag);
+  };
+
+  const closeToast = () => {
+    setShowToast(false);
+    setToastMessage("");
+  };
 
   useEffect(() => {
     const result = VALID_EMAIL.test(email.value);
-    setEmail({ ...email, isValidate: result });
-    console.log("email", email.isValidate);
+    setEmail({ ...email, isValid: result });
+    console.log("email", email.isValid);
   }, [email.value]);
 
   return (
     <div className={style.container}>
-      <NavigationBar />
+      <NavigationBar
+        toastMessage={toastMessage}
+        showToast={showToast}
+        closeToast={closeToast}
+        flag={flag}
+      />
       <div className={style.formControl}>
         <div className={style.title}>
           <h3>ورود از طریق ایمیل</h3>
@@ -36,22 +53,10 @@ function LoginForm() {
           setEmail={setEmail}
           password={password}
           setPassword={setPassword}
-          form={style.form}
-          btnSubmit={style.btnSubmit}
-          optionItem={style.optionItem}
-        />
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={true}
-          newestOnTop={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-          closeButton={false}
-          style={{ fontSize: "14px", lineHeight: "24px" }}
+          classNameForm={style.form}
+          classNameBtnSubmit={style.btnSubmit}
+          classNameOptionItem={style.optionItem}
+          showToastNotification={showToastNotification}
         />
       </div>
     </div>
